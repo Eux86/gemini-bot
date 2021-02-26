8ed3411ad3121233b7a0d9d4560cdb88467605be
Eugenio Di Tullio <ditullioeugenio@gmail.com>
2021-02-26 23:07:02 +0000

    separated test and prod

--- src/commands/maps-info.ts
+++ src/commands/maps-info.ts
@@ -2,7 +2,7 @@ import { ICommand } from ".";
 import { getService, Services } from "../service-factory";
 
 const mapsInfo: ICommand = {
-  name: 'map-info',
+  name: 'maps',
   description: 'Returns info about the available servers maps',
   command: async (msg: any, args: any) => {
       const mapsInfoService = getService(Services.MapsInfo);
